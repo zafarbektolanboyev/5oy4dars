5c6c4fbfd1f84845c7bd420685824c0c4da87386
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-04-04 16:16:45 +0500

    jshtml

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-// let button1 = document.querySelector("#btn");
-// let button2 = document.querySelector("#btn1");
-// let section = document.querySelector("#section");
+let button1 = document.querySelector("#btn");
+let button2 = document.querySelector("#btn1");
+let section = document.querySelector("#section");
 
-// function btnClick(){
-//     let h2 = button1.nextElementSibling;
-//     button2.disabled = false;
-//     if(+h2.textContent == 10){
-//         button1.setAttribute("disabled", true);
-//     }else{
-//         h2.textContent++;
-//     }
-// }
-// function btn1Click(){
-//     let h2 = button2.previousElementSibling;
-//     button1.disabled = false;
-//     if(-h2.textContent == 0){
-//     }else{
-//         h2.textContent--;
-//     }
-// }
+function btnClick(){
+    let h2 = button1.nextElementSibling;
+    button2.disabled = false;
+    if(+h2.textContent == 10){
+        button1.setAttribute("disabled", true);
+    }else{
+        h2.textContent++;
+    }
+}
+function btn1Click(){
+    let h2 = button2.previousElementSibling;
+    button1.disabled = false;
+    if(-h2.textContent == 0){
+    }else{
+        h2.textContent--;
+    }
+}
 
-// button1.setAttribute("onclick", "btnClick()");
-// button2.setAttribute("onclick", "btn1Click()");
+button1.setAttribute("onclick", "btnClick()");
+button2.setAttribute("onclick", "btn1Click()");
 
 
-// // section.style.background = "#000";
-// // Math.random(
-// function RandomColor(){
-//     let randomNum1 = Math.floor(Math.random() * 255)
-//     let randomNum2 = Math.floor(Math.random() * 255)
-//     let randomNum3 = Math.floor(Math.random() * 255)
-//     return `rgb(${randomNum1}, ${randomNum2}, ${randomNum3})`;
-// }
-// section.style.backgroundColor = RandomColor();
-// section.style.backgroundImage = `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`;
-// btnClick("backgroundImage");
-// btn1Click("backgrounImage");
+// section.style.background = "#000";
+// Math.random(
+function RandomColor(){
+    let randomNum1 = Math.floor(Math.random() * 255)
+    let randomNum2 = Math.floor(Math.random() * 255)
+    let randomNum3 = Math.floor(Math.random() * 255)
+    return `rgb(${randomNum1}, ${randomNum2}, ${randomNum3})`;
+}
+section.style.backgroundColor = RandomColor();
+section.style.backgroundImage = `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`;
+btnClick("backgroundImage");
+btn1Click("backgrounImage");
 
 
 
